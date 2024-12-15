d6071e00fb2f321bc5241ba004c7fd68eb9cafa6
af5rany <58744028+af5rany@users.noreply.github.com>
2024-12-15 19:46:14 +0200

    sync 2

--- backend/src/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserResolver } from './user.resolver';
-import { UserService } from './user.service';
-
-@Module({
-  providers: [UserResolver, UserService],
-  // exports: [UserService], // Export if needed in other modules
-})
-export class UserModule {}
